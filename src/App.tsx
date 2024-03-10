e2aefea305bc0c3bcc7b0ef3049fae72cadaea5e
Kotaro-Miyagi <miyagi.bad@icloud.com>
2024-03-10 21:33:29 +0900

    add input form

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="formContainer">
-
+      <form>
+        <h1>Login Form</h1>
+          <hr />
+          <div className="uiForm"></div>
+          <div className="formField">
+              <label>User Name</label>
+              <input type="text" />
+              <label>E-mail</label>
+              <input type="text" />
+              <label>Password</label>
+              <input type="text" />
+          </div>
+      </form>
     </div>
   );
 }
