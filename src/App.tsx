82c883b037331964c1c1b9fe4322da9b8d4d2cd1
Kotaro-Miyagi <miyagi.bad@icloud.com>
2024-03-10 22:58:18 +0900

    complete css

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="formContainer">
-      <form>
-        <h1>Login Form</h1>
-          <hr />
-          <div className="uiForm"></div>
-          <div className="formField">
-              <label>User Name</label>
-              <input type="text" placeholder="user"/>
-              <label>E-mail</label>
-              <input type="text" placeholder="e-mail"/>
-              <label>Password</label>
-              <input type="text" placeholder="password"/>
-          </div>
-      </form>
-    </div>
-  );
+    return (
+        <div className="formContainer">
+            <form>
+                <h1>Login Form</h1>
+                <hr />
+                <div className="uiForm">
+                    <div className="formField">
+                        <label>User Name</label>
+                        <input type="text" placeholder="user" name="username" />
+                    </div>
+                    <div className="formField">
+                        <label>E-mail Address</label>
+                        <input type="text" placeholder="e-mail" name="e-mail" />
+                    </div>
+                    <div className="formField">
+                        <label>Password</label>
+                        <input type="password" placeholder="password" name="password" />
+                    </div>
+                    <button className="submitButton">Login</button>
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default App;
