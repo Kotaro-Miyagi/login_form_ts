79741b26a23c86fc9ce806c3dca6d6fea000cddb
Kotaro-Miyagi <miyagi.bad@icloud.com>
2024-03-10 23:15:49 +0900

    add onchange

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
 import './App.css';
+import {useState} from "react";
+
+interface FormValues {
+    username: string;
+    mailAddress: string;
+    password: string;
+}
 
 function App() {
+    const initialValues = {
+        username: "",
+        mailAddress: "",
+        password: ""
+    }
+    const [formValues, setFormValues] = useState<FormValues>()
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value} = e.target
+        setFormValues({ ...formValues, [name]: value })
+        console.log(formValues)
+    }
+
     return (
         <div className="formContainer">
             <form>
@@ -9,15 +29,15 @@ function App() {
                 <div className="uiForm">
                     <div className="formField">
                         <label>User Name</label>
-                        <input type="text" placeholder="user" name="username" />
+                        <input type="text" placeholder="user" name="username" onChange={(e) => handleChange(e)}/>
                     </div>
                     <div className="formField">
                         <label>E-mail Address</label>
-                        <input type="text" placeholder="e-mail" name="e-mail" />
+                        <input type="text" placeholder="e-mail" name="mailAddress" onChange={(e) => handleChange(e)}/>
                     </div>
                     <div className="formField">
                         <label>Password</label>
-                        <input type="password" placeholder="password" name="password" />
+                        <input type="password" placeholder="password" name="password" onChange={(e) => handleChange(e)}/>
                     </div>
                     <button className="submitButton">Login</button>
                 </div>
