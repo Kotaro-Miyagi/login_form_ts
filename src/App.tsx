91ee4b2a5054f90ec5010cccddf4c06a93e7c203
Kotaro-Miyagi <miyagi.bad@icloud.com>
2024-03-12 04:05:15 +0900

    completed loginform

--- src/App.tsx
+++ src/App.tsx
@@ -14,40 +14,47 @@ function App() {
         password: ""
     };
 
-    const [formValues, setFormValues] = useState<FormValues>(initialValues);
+    const [formValues, setFormValues] = useState<FormValues>(initialValues)
+    const [formErrors, setFormErrors] = useState<Partial<FormValues>>({})
+    const [isSubmit, setIsSubmit] = useState<boolean>(false)
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setFormValues((prevFormValues) => ({ ...prevFormValues, [name]: value }));
+        const { name, value } = e.target
+        setFormValues((prevFormValues) => ({ ...prevFormValues, [name]: value }))
+        setFormErrors((prevFormErrors) => ({ ...prevFormErrors, [name]: "" }))
     };
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const errors = validate(formValues);
-        if (Object.keys(errors).length === 0) {
-            console.log("Form is valid. Submitting...");
-        } else {
-            console.error("Form validation failed:", errors);
-        }
-    };
+        e.preventDefault()
+        // const errors = validate(formValues)
+        // if (Object.keys(errors).length === 0) {
+        //     console.log("Form is valid. Submitting...")
+        //     // Add your form submission logic here
+        // } else　 {
+        //     console.error("Form validation failed:", errors)
+        //     setFormErrors(errors)
+        // }　
+        setFormErrors(validate(formValues))
+        setIsSubmit(true)
+    }
 
     const validate = (values: FormValues): Partial<FormValues> => {
-        const errors: Partial<FormValues> = {};
-        const regex = /^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/;
+        const errors: Partial<FormValues> = {}
+        const regex = /^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/
         if (!values.username) {
-            errors.username = "Please enter your username";
+            errors.username = "Please enter your username"
         }
         if (!values.mailAddress) {
-            errors.mailAddress = "Please enter your mail address";
+            errors.mailAddress = "Please enter your mail address"
         } else if (!regex.test(values.mailAddress)) {
-            errors.mailAddress = "Please enter a valid email address";
+            errors.mailAddress = "Please enter a valid email address"
         }
         if (!values.password) {
-            errors.password = "Please enter your password";
+            errors.password = "Please enter your password"
         } else if (values.password.length < 4 || values.password.length > 15) {
-            errors.password = "Please enter a password of 4 to 15 characters";
+            errors.password = "Please enter a password of 4 to 15 characters"
         }
-        return errors;
+        return errors
     };
 
     return (
@@ -58,21 +65,27 @@ function App() {
                 <div className="uiForm">
                     <div className="formField">
                         <label>User Name</label>
-                        <input type="text" placeholder="user" name="username" onChange={handleChange} />
+                        <input type="text" placeholder="user" name="username" onChange={handleChange}/>
+                        <p className="errorMsg">{formErrors.username}</p>
                     </div>
                     <div className="formField">
                         <label>E-mail Address</label>
-                        <input type="text" placeholder="e-mail" name="mailAddress" onChange={handleChange} />
+                        <input type="text" placeholder="e-mail" name="mailAddress" onChange={handleChange}/>
+                        <p className="errorMsg">{formErrors.mailAddress}</p>
                     </div>
                     <div className="formField">
                         <label>Password</label>
-                        <input type="password" placeholder="password" name="password" onChange={handleChange} />
+                        <input type="password" placeholder="password" name="password" onChange={handleChange}/>
+                        <p className="errorMsg">{formErrors.password}</p>
                     </div>
                     <button className="submitButton" type="submit">Login</button>
+                    {Object.keys(formErrors).length === 0 && isSubmit && (
+                        <div className="msgOk">Login successful</div>
+                    )}
                 </div>
             </form>
         </div>
     );
 }
 
-export default App;
+export default App
